f5c95fc12bc1f1e196a6b76999b8148061b560bc
marcinczachor <mar.czachor@gmail.com>
2021-02-14 01:10:36 +0100

    Changed basic setup

--- src/js/bricks.js
+++ src/js/bricks.js
@@ -1,123 +1,133 @@
 import qs from './helper';
 
 const bricks = (function () {
-    'use strict';
+	'use strict';
 
-    const board = qs('.board');
-    let wallOfBricks = Array(5).fill().map(cell => Array(3).fill(null));
+	const board = qs('.board');
+	let wallOfBricks = Array(5)
+		.fill()
+		.map((cell) => Array(3).fill(null));
 
-    class Brick {
-        constructor() {
-            this.x = 0;
-            this.y = 0;
-            this.width = 228;
-            this.height = 45;
-            this.offsetLeft = 40;
-            this.offsetTop = 40;
-            this.status = 1;
-        }
-    }
+	class Brick {
+		constructor() {
+			this.x = 0;
+			this.y = 0;
+			this.width = 228;
+			this.height = 45;
+			this.offsetLeft = 40;
+			this.offsetTop = 40;
+			this.status = 1;
+		}
+	}
 
-    class BrickWall {
+	class BrickWall {
+		static fillWall() {
+			return wallOfBricks.map((array) => array.map((cell) => new Brick()));
+		}
 
-        static fillWall() {
-            return wallOfBricks.map(array => array.map(cell => new Brick()));
-        }
+		constructor(ball, paddle, settings, context) {
+			this.ball = ball;
+			this.paddle = paddle;
+			this.settings = settings;
+			this.context = context;
+			this.padding = 15;
+			this.columns = 5;
+			this.rows = 3;
+			this.wall = BrickWall.fillWall();
+		}
 
-        constructor(ball, paddle, settings, context) {
-            this.ball = ball;
-            this.paddle = paddle;
-            this.settings = settings;
-            this.context = context;
-            this.padding = 15;
-            this.columns = 5;
-            this.rows = 3;
-            this.wall = BrickWall.fillWall();
-        }
+		//Draw wall of bricks set in this.wall property
+		drawBricks() {
+			const width = this.wall[0][0].width;
+			const height = this.wall[0][0].height;
+			const offsetLeft = this.wall[0][0].offsetLeft;
+			const offsetTop = this.wall[0][0].offsetTop;
+			let stX, stY;
 
-        //Draw wall of bricks set in this.wall property
-        drawBricks() {
-            const width = this.wall[0][0].width;
-            const height = this.wall[0][0].height;
-            const offsetLeft = this.wall[0][0].offsetLeft;
-            const offsetTop = this.wall[0][0].offsetTop;
-            let stX, stY;
+			this.wall.map((array, i) =>
+				array.map((cell, j) => {
+					if (this.wall[i][j].status === 1) {
+						stX = i * (width + this.padding) + offsetLeft;
+						stY = j * (height + this.padding) + offsetTop;
 
-            this.wall.map((array, i) => array.map((cell, j) => {
-                if (this.wall[i][j].status === 1) {
-                    stX = i * (width + this.padding) + offsetLeft;
-                    stY = j * (height + this.padding) + offsetTop;
+						this.wall[i][j].x = stX;
+						this.wall[i][j].y = stY;
 
-                    this.wall[i][j].x = stX;
-                    this.wall[i][j].y = stY;
+						this.context.fillStyle = 'rgb(92, 100, 174)';
+						this.context.beginPath();
+						this.context.fillRect(stX, stY, width, height);
+						this.context.closePath();
+					}
+				})
+			);
+		}
 
-                    this.context.fillStyle = 'rgb(92, 100, 174)';
-                    this.context.beginPath();
-                    this.context.fillRect(stX, stY, width, height);
-                    this.context.closePath();
-                }
-            }));
-        }
+		//Set win condition for the game
+		winCondition() {
+			const { level, score } = this.settings;
 
-        //Set win condition for the game
-        winCondition() {
-            const { level, score } = this.settings;
+			if (level >= 3 && score === 450) {
+				alert('CONGRATULATIONS! YOU FINISHED THE GAME!');
+				document.location.reload();
+			} else {
+				alert('CONGRATULATIONS! YOU FINISHED LEVEL: ' + level);
+				this.settings.level++;
+				this.settings.lives++;
+			}
+		}
 
-            if (level >= 3 && score === 450) {
-                alert('CONGRATULATIONS! YOU FINISHED THE GAME!');
-                document.location.reload();
-            } else {
-                alert('CONGRATULATIONS! YOU FINISHED LEVEL: ' + level);
-                this.settings.level++;
-                this.settings.lives++;
-            }
-        }
+		//Set new level for the game
+		setNewLevel() {
+			const { level, score } = this.settings;
 
-        //Set new level for the game
-        setNewLevel() {
-            const { level, score } = this.settings;
+			if (level * 150 === score) {
+				this.ball.x = board.width / 2;
+				this.ball.y = board.height - 35;
+				this.paddle.x = (board.width - 150) / 2;
+				this.paddle.y = board.height - 20;
 
-            if (level * 150 === score) {
-                this.ball.x = board.width / 2;
-                this.ball.y = board.height - 35;
-                this.paddle.x = (board.width - 150) / 2;
-                this.paddle.y = board.height - 20;
+				this.ball.dX = this.ball.dX < 0 ? -this.ball.dX : this.ball.dX;
+				this.ball.dY =
+					this.ball.dY < 0 ? -this.ball.dY + 2.5 : this.ball.dY + 2.5;
 
-                this.ball.dX = (this.ball.dX < 0) ? -this.ball.dX : this.ball.dX;
-                this.ball.dY = (this.ball.dY < 0) ? -this.ball.dY + 2.5 : this.ball.dY + 2.5;
+				this.winCondition();
+				this.wall = BrickWall.fillWall();
+			}
+		}
 
-                this.winCondition();
-                this.wall = BrickWall.fillWall();
-            }
-        }
+		//Detect collision with each brick so that wall can be redraw
+		detectColision() {
+			const width = this.wall[0][0].width;
+			const height = this.wall[0][0].height;
+			let brick;
 
-        //Detect collision with each brick so that wall can be redraw
-        detectColision() {
-            const width = this.wall[0][0].width;
-            const height = this.wall[0][0].height;
-            let brick;
+			this.drawBricks();
 
-            this.drawBricks();
+			this.wall.map((array, i) =>
+				array.map((cell, j) => {
+					if (this.wall[i][j].status === 1) {
+						brick = this.wall[i][j];
 
-            this.wall.map((array, i) => array.map((cell, j) => {
-                if (this.wall[i][j].status === 1) {
-                    brick = this.wall[i][j];
+						if (
+							this.ball.x > brick.x &&
+							this.ball.x < brick.x + width &&
+							this.ball.y > brick.y &&
+							this.ball.y < brick.y + height
+						) {
+							this.ball.dY = -this.ball.dY;
+							brick.status = 0;
+							this.settings.score += 10;
+							this.setNewLevel();
+						}
+					}
+				})
+			);
+		}
+	}
 
-                    if (this.ball.x > brick.x && this.ball.x < brick.x + width && this.ball.y > brick.y && this.ball.y < brick.y + height) {
-                        this.ball.dY = -this.ball.dY;
-                        brick.status = 0;
-                        this.settings.score += 10;
-                        this.setNewLevel();
-                    }
-                }
-            }));
-        }
-    }
-
-    return {
-        class: BrickWall
-    };
-
+	return {
+		class: BrickWall,
+	};
 })();
 
 export default bricks;
--- src/js/timer.js
+++ src/js/timer.js
@@ -1,41 +1,40 @@
 import qs from './helper';
 
 const settings = (function () {
-    'use strict';
+	'use strict';
 
-    class Settings {
-        constructor() {
-            this.level = 1;
-            this.score = 0;
-            this.lives = 3;
-            this.timer = 0;
-            this.gameLevel = qs('.game-level');
-            this.playerScore = qs('.player-score');
-            this.playerLives = qs('.player-lives');
-            this.setTimer = qs('.game-timer');
-        }
+	class Settings {
+		constructor() {
+			this.level = 1;
+			this.score = 0;
+			this.lives = 3;
+			this.timer = 0;
+			this.gameLevel = qs('.game-level');
+			this.playerScore = qs('.player-score');
+			this.playerLives = qs('.player-lives');
+			this.setTimer = qs('.game-timer');
+		}
 
-        //Fill basic settings of the game
-        fillStats() {
-            this.gameLevel.innerHTML = this.level;
-            this.playerScore.innerHTML = this.score;
-            this.playerLives.innerHTML = this.lives;
-        }
+		//Fill basic settings of the game
+		fillStats() {
+			this.gameLevel.innerHTML = this.level;
+			this.playerScore.innerHTML = this.score;
+			this.playerLives.innerHTML = this.lives;
+		}
 
-        //Increment seconds on the cloak        
-        changeTime() {
-            this.setTimer.innerHTML = this.timer + ' [s]';
-            this.timer++;
-            setTimeout(() => {
-                this.changeTime();
-            }, 1000);
-        }
-    }
+		//Increment seconds on the cloak
+		changeTime() {
+			this.setTimer.innerHTML = this.timer + ' [s]';
+			this.timer++;
+			setTimeout(() => {
+				this.changeTime();
+			}, 1000);
+		}
+	}
 
-    return {
-        class: Settings
-    };
-
+	return {
+		class: Settings,
+	};
 })();
 
 export default settings;
