0138822026e121e00a6db62d754ef722f845bf08
Marcin Czachor <mar.czachor@gmail.com>
2021-10-05 20:59:47 +0200

    Basic setup improvements

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,27 @@
+import css from 'rollup-plugin-css-only';
+import livereload from 'rollup-plugin-livereload';
 import resolve from 'rollup-plugin-node-resolve';
+import serve from 'rollup-plugin-serve';
 import typescript from 'rollup-plugin-typescript2';
 
 export default {
   input: 'src/index.ts',
   output: {
     file: 'dist/bundle.js',
-    format: 'iife',
+    format: 'esm',
   },
   plugins: [
+    serve({
+      contentBase: 'dist',
+      open: true,
+      host: 'localhost',
+      port: 3000,
+    }),
+    livereload({ watch: 'dist' }),
     resolve({
       extensions: ['.js', '.ts'],
     }),
+    css({ output: 'bundle.css' }),
     typescript({
       module: 'ESNext',
     }),
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-alert('bundle');
+import './js/app';
