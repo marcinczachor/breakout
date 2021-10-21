38355df2f0eca5e689219582ff942558fe44f6c6
Marcin Czachor <mar.czachor@gmail.com>
2021-10-21 22:47:06 +0200

    Added content to gameboard (#10)

---
+++ ui/src/pages/gameboard/board/Board.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { colors } from 'constants/colors';
+</script>
+
+<canvas
+  class="board"
+  style="--color-primary: {colors.turquoise[400]};
+        --color-white: {colors.white[400]}"
+  width="1200"
+  height="580"
+/>
+
+<style>
+  .board {
+    max-width: 100%;
+    width: 100%;
+    background: var(--color-white);
+    border: 1px solid var(--color-primary);
+  }
+</style>
---
+++ ui/src/pages/gameboard/score/Score.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { colors } from 'constants/colors';
+
+  export let score = 0;
+  export let timer = 0;
+</script>
+
+<div
+  class="score"
+  style="--color-primary: {colors.turquoise[400]};
+        --color-white: {colors.white[400]}"
+>
+  <ul class="list">
+    <li class="row">
+      <span>Score:</span>
+      <span>{score}</span>
+    </li>
+    <li class="row">
+      <span>Timer:</span>
+      <span>{timer}</span>
+    </li>
+  </ul>
+</div>
+
+<style>
+  .score {
+    max-width: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0.75rem 2rem;
+    background: var(--color-primary);
+  }
+
+  .list {
+    min-width: 12.5rem;
+    display: flex;
+    padding: 0;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .row {
+    min-width: 5rem;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  span {
+    color: var(--color-white);
+  }
+</style>
